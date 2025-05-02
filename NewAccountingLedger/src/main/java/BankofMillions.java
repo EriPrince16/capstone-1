732c157bf6004e9c9d6735f04eb88b241e777045
Million Bereket <million.bereket16@gmail.com>
2025-05-01 22:21:31 -0500

    "Wrote down notes!"

--- NewAccountingLedger/src/main/java/BankofMillions.java
+++ NewAccountingLedger/src/main/java/BankofMillions.java
@@ -10,51 +10,78 @@ import java.util.ArrayList;
 import java.io.FileReader;
 import java.util.Scanner;
 
+// This is the name of the class. A class is like a blueprint for creating objects.
+// It can contain variables (called fields) and actions (called methods).
 public class BankofMillions {
+
+    // This is a field. A field is a variable that belongs to the class.
+    // We are creating a Scanner object. An object is something made from a class.
+    // Scanner helps us get input from the keyboard.
     static Scanner scanner = new Scanner(System.in);
 
+    // This is the main method. A method is a block of code that does something.
+    // The main method is where the program starts when we run it.
     public static void main(String[] args) {
-        ArrayList<Transaction> ledger; // Created an ArrayList with the type Transaction and named it ledger
-//        ledger = loadLedger(); // Assigning ledger to the ArrayList that returns loadLedger
+        // Creating a variable named ledger. It is an ArrayList that will store many Transaction objects.
+        // An ArrayList is like a flexible list that can grow or shrink as needed.
+        ArrayList<Transaction> ledger;
+
+        // This line is commented out for now. It would normally load all previous transactions into the ledger list.
+        // ledger = loadLedger();
+
+        // This line calls the displayHome method to show the home screen of the bank app.
         displayHome();
-
-
     }
 
-    // Show the Home Menu
+    // This method shows the Home Menu where the user can choose actions.
+    // It uses a loop to keep showing the menu until the user chooses to exit.
     public static void displayHome() {
+        // Creating a boolean variable to control the loop
         boolean keepGoing = true;
+
+        // This is a while loop. It keeps running as long as keepGoing is true.
         while (keepGoing) {
+            // Display menu options to the user
             System.out.println("\n===== Welcome to Bank of Millions =====");
             System.out.println("D) Add Deposit");
             System.out.println("P) Make Payment");
             System.out.println("L) View Ledger");
             System.out.println("X) Exit");
             System.out.print("Enter choice: ");
-            Scanner scanner = new Scanner(System.in);
+
+            // Getting user input and turning it into uppercase letters
             String userChoice = scanner.nextLine().toUpperCase();
+
+            // Show what the user picked
             System.out.println("You have selected: " + userChoice);
 
+            // This is an if-else statement to check what the user chose
             if (userChoice.equals("D")) {
+                // If the user picked D, we call the method to add a deposit
                 addTransaction(true);
             } else if (userChoice.equals("P")) {
+                // If the user picked P, we call the method to make a payment
                 addTransaction(false);
             } else if (userChoice.equals("L")) {
+                // If the user picked L, we show the ledger menu
                 ledgerMenu();
             } else if (userChoice.equals("X")) {
+                // If the user picked X, we break the loop and exit the app
                 break;
             } else {
+                // If the user typed something else, we show an error
                 System.out.println("Invalid input! Try again.");
             }
         }
-
-
     }
 
-    // Method to deposit or make a payment
+    // This method is used to add a transaction (deposit or payment)
+    // A parameter is something we pass into a method to use. isDeposit tells us if it's a deposit or not.
     public static void addTransaction(boolean isDeposit) {
+        // This is a String variable that holds the path to the file where we save transactions
         String filePath = "src/main/resources/ledger.csv";
 
+        // Asking the user to enter transaction information
         System.out.println("Enter description of the transaction: ");
         String description = scanner.nextLine();
         System.out.println("Enter the vendor: ");
@@ -62,35 +89,35 @@ public class BankofMillions {
         System.out.println("Enter the amount: ");
         double amount = scanner.nextDouble();
 
+        // If the transaction is a payment, we make the amount negative
         if (!isDeposit) {
             amount = -amount;
-
         }
 
-
+        // Getting the current date and time
         LocalDateTime localDateTime = LocalDateTime.now();
+        // Formatting the date and time in a readable way
         DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd|HH:mm:ss");
         String logDateTime = localDateTime.format(dateTimeFormatter);
 
-        // Format the transaction line
+        // Putting all the transaction info into one line to save in the file
         String transactionLine = description + "|" + vendor + "|" + amount;
 
+        // Try to write the transaction to the file
         try {
-            FileWriter fileWriter = new FileWriter(filePath, true);
-            fileWriter.write("\n" + logDateTime + "|" + transactionLine);
-            fileWriter.close();
-            String line;
+            FileWriter fileWriter = new FileWriter(filePath, true); // true means we are adding to the file
+            fileWriter.write("\n" + logDateTime + "|" + transactionLine); // writing the data to the file
+            fileWriter.close(); // closing the file after writing
             System.out.println("Your transaction was successful!");
-
         } catch (Exception e) {
+            // If something goes wrong, we stop the program and show the error
             throw new RuntimeException(e);
         }
-
     }
 
-
+    // This method shows the ledger menu where the user can view different types of transactions
     public static void ledgerMenu() {
-        while (true) {
+        while (true) { // Keep showing the menu until the user goes back
             System.out.println("\n===== Ledger Menu =====");
             System.out.println("A) All Transactions");
             System.out.println("D) Deposits Only");
@@ -98,52 +125,51 @@ public class BankofMillions {
             System.out.println("R) Reports");
             System.out.println("H) Home");
             System.out.print("Enter your choice: ");
+
+            // Read the user's choice
             String userChoice = scanner.nextLine().toUpperCase();
 
-            if (userChoice.equals("A")) {
-                displayTransaction(userChoice);
-            } else if (userChoice.equals("D")) {
-                displayTransaction(userChoice);
-
-            } else if (userChoice.equals("P")) {
-                displayTransaction(userChoice);
-
+            // Based on the choice, call the correct method
+            if (userChoice.equals("A") || userChoice.equals("D") || userChoice.equals("P")) {
+                displayTransaction(userChoice); // Show matching transactions
             } else if (userChoice.equals("R")) {
-                displayReports();
+                displayReports(); // Go to reports menu
             } else if (userChoice.equals("H")) {
-                displayHome();
-
+                displayHome(); // Go back to main home menu
             }
         }
     }
 
-
+    // This method shows transactions based on the user's choice
     public static void displayTransaction(String userChoice) {
-        if (userChoice.equals("A")) { // If the user chose to view all transactions
-            for (Transaction transactions : loadLedger()) { // For each transactions of the ArrayList (loadLedger())
-                System.out.println(transactions.toString()); // Print out all transactions
+        // If the user picked A, we show all transactions
+        if (userChoice.equals("A")) {
+            // This is a for-each loop. It goes through every item in the list one by one.
+            for (Transaction transactions : loadLedger()) {
+                System.out.println(transactions.toString()); // Print each transaction
             }
-        } else if (userChoice.equals("D")) { // If the user chose to view all transactions by Deposit
-            for (Transaction transactions : loadLedger()) { // For each transaction of the ArrayList (loadLedger())
-                String [] parts = transactions.toString().split("\\|"); //Converts each transactions into a string and split them by "\\|" into an Array
-                double amount = Double.parseDouble(parts[4]); // Grabbed the 4th index and turned it from a String into a double
-                if (amount > 0) { // If the amount is a positive number
-                    System.out.println(transactions.toString()); // Print out all transactions with a postive amount
+        } else if (userChoice.equals("D")) {
+            // Show only deposit transactions
+            for (Transaction transactions : loadLedger()) {
+                String[] parts = transactions.toString().split("\\|"); // Split the string into pieces
+                double amount = Double.parseDouble(parts[4]); // Turn the amount into a number
+                if (amount > 0) { // Only show if the amount is positive
+                    System.out.println(transactions.toString());
                 }
             }
-        } else if (userChoice.equals("P")) { // If the user chose to view all transactions by Payments
-            for (Transaction transactions : loadLedger()) { // For each transaction of the ArrayList (loadLedger())
-                String [] parts = transactions.toString().split("\\|"); // Converts each transactions into a string and split them by "\\|" into an Array
-                double amount = Double.parseDouble(parts[4]); // Grabbed the 4th index and turned it from a String into a double
-                if (amount < 0) { // If the amount is a negative number
-                    System.out.println(transactions.toString()); // Print out all transactions with a negative amount
+        } else if (userChoice.equals("P")) {
+            // Show only payment transactions
+            for (Transaction transactions : loadLedger()) {
+                String[] parts = transactions.toString().split("\\|");
+                double amount = Double.parseDouble(parts[4]);
+                if (amount < 0) { // Only show if the amount is negative
+                    System.out.println(transactions.toString());
                 }
             }
         }
-
-
     }
 
+    // This method shows the Reports Menu
     public static void displayReports() {
         while (true) {
             System.out.println("\n===== Reports =====");
@@ -157,61 +183,61 @@ public class BankofMillions {
             String choice = scanner.nextLine();
 
             if (choice.equals("1")) {
-
+                showMonthToDate(); // Call method to show current month transactions
             } else if (choice.equals("2")) {
-                showMonthToDate();
-
+                // (Code for this will be added later)
             } else if (choice.equals("3")) {
-
+                // (Code for this will be added later)
             } else if (choice.equals("4")) {
-
+                // (Code for this will be added later)
             } else if (choice.equals("5")) {
-
+                // (Code for this will be added later)
             } else if (choice.equals("0")) {
-                break;
+                break; // Exit the reports menu
             }
         }
     }
 
+    // This method will show transactions for the current month (to be completed later)
     public static void showMonthToDate() {
         System.out.println("\n===== Month to Date =====");
-
-
-
+        // (More code will go here in the future)
     }
 
-
-    public static ArrayList<Transaction> loadLedger() { // Created a method to
+    // This method loads all transactions from the file and puts them into an ArrayList
+    public static ArrayList<Transaction> loadLedger() {
+        // Create an empty list to store all transactions
         ArrayList<Transaction> ledger = new ArrayList<Transaction>();
-        String filePath = "src/main/resources/ledger.csv";
+        String filePath = "src/main/resources/ledger.csv"; // Location of the file
         DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
         DateTimeFormatter timeFormatter = DateTimeFormatter.ofPattern("HH:mm:ss");
-
 
         try {
             FileReader fileReader = new FileReader(filePath);
             BufferedReader bufferedReader = new BufferedReader(fileReader);
             String line;
 
+            // This while loop keeps running as long as there are lines to read
             while ((line = bufferedReader.readLine()) != null) {
+                // Split each line into parts based on the "|" symbol
                 String[] parts = line.split("\\|");
-                LocalDate date = LocalDate.parse(parts[0], dateFormatter);
-                LocalTime time = LocalTime.parse(parts[1], timeFormatter);
+                LocalDate date = LocalDate.parse(parts[0], dateFormatter); // Convert date from String to Date
+                LocalTime time = LocalTime.parse(parts[1], timeFormatter); // Convert time from String to Time
                 String description = parts[2];
                 String vendor = parts[3];
                 double amount = Double.parseDouble(parts[4]);
+
+                // Create a new Transaction object using the parts
                 Transaction newTransaction = new Transaction(date, time, description, vendor, amount);
+
+                // Add this transaction to the ledger list
                 ledger.add(newTransaction);
-
-
             }
-
         } catch (IOException e) {
-            System.out.println("Error!");
+            System.out.println("Error!"); // Show error if something goes wrong
         }
 
-
+        // Return the list of transactions
         return ledger;
     }
-
 }
--- NewAccountingLedger/src/main/java/Transaction.java
+++ NewAccountingLedger/src/main/java/Transaction.java
@@ -1,64 +1,86 @@
 import java.time.LocalDate;
 import java.time.LocalTime;
 
+// This is the Transaction class. A class is a blueprint for creating objects.
 public class Transaction {
-     LocalDate date;
-     LocalTime time;
-     String description;
-     String vendor;
-     double amount;
 
+    // These are fields. Fields are variables that store data inside the object.
+    // Each field represents a piece of information about a transaction.
+    LocalDate date;        // Date of the transaction
+    LocalTime time;        // Time of the transaction
+    String description;    // Description of the transaction
+    String vendor;         // Vendor name involved in the transaction
+    double amount;         // The amount of the transaction (positive for deposits, negative for payments)
+
+    // This is a constructor. A constructor is a special method that creates an object with specific values.
+    // When you create a Transaction object, you need to provide a date, time, description, vendor, and amount.
     public Transaction(LocalDate date, LocalTime time, String description, String vendor, double amount) {
-        this.date = date;
-        this.time = time;
-        this.description = description;
-        this.vendor = vendor;
-        this.amount = amount;
+        // The 'this' keyword refers to the current object, and the parameter names (e.g., 'date') are the values passed in.
+        this.date = date;          // Sets the date for this transaction
+        this.time = time;          // Sets the time for this transaction
+        this.description = description; // Sets the description of the transaction
+        this.vendor = vendor;      // Sets the vendor involved in the transaction
+        this.amount = amount;      // Sets the amount of the transaction (deposit or payment)
     }
 
+    // These are getter methods. A getter method allows us to access (read) the value of a private field.
+    // We use these methods to get the value of the fields outside the class.
+
     public double getAmount() {
+        // This returns the value stored in the 'amount' field
         return amount;
     }
 
+    // These are setter methods. A setter method allows us to change (update) the value of a field.
+    // We use these methods to set the value of fields outside the class.
+
     public void setAmount(double amount) {
+        // This sets the value of the 'amount' field to the new value passed as a parameter
         this.amount = amount;
     }
 
+    // Getter and setter methods for 'vendor' field
     public String getVendor() {
-        return vendor;
+        return vendor; // This returns the value stored in the 'vendor' field
     }
 
     public void setVendor(String vendor) {
-        this.vendor = vendor;
+        this.vendor = vendor; // This sets the 'vendor' field to the new value
     }
 
+    // Getter and setter methods for 'description' field
     public String getDescription() {
-        return description;
+        return description; // This returns the value stored in the 'description' field
     }
 
     public void setDescription(String description) {
-        this.description = description;
+        this.description = description; // This sets the 'description' field to the new value
     }
 
+    // Getter and setter methods for 'time' field
     public LocalTime getTime() {
-        return time;
+        return time; // This returns the value stored in the 'time' field
     }
 
     public void setTime(LocalTime time) {
-        this.time = time;
+        this.time = time; // This sets the 'time' field to the new value
     }
 
+    // Getter and setter methods for 'date' field
     public LocalDate getDate() {
-        return date;
+        return date; // This returns the value stored in the 'date' field
     }
 
     public void setDate(LocalDate date) {
-        this.date = date;
+        this.date = date; // This sets the 'date' field to the new value
     }
 
-
+    // This is the toString method. It converts the object into a string that is easy to read.
+    // The 'toString' method is used when you want to print out the object in a readable format.
     @Override
     public String toString() {
+        // This combines the fields into a single string, separating them with "|"
+        // Example: "2025-05-01|12:30:00|Purchase at store|Store A|50.0"
         return date + "|" + time + "|" + description + "|" + vendor + "|" + amount;
     }
 }
